0c7d89515c0aa26e32b4fa167b8d751531a9c4e9
Billy Eskein <billyeskein@gmail.com>
2025-01-10 18:58:09 +0300

    Update src/index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    let form = document.querySelector('form')
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        handleTasks(e.target['new-task'].value);
+        form.reset()
+    })
 });
+function handleTasks(task) {
+    const li = document.createElement('li');
+    li.textContent = `${task} `;
+    document.getElementById('tasks').appendChild(li);
+const btn = document.createElement('button');
+btn.textContent = 'x';
+li.appendChild(btn);
+btn.addEventListener('click', handleDelete);
+}
+function handleDelete(e) {
+    e.target.parentNode.remove()
+}
